ab16df5b0aaee3b8453a062c78b9602a452223ef
Nathan <nathan.ly.tang@gmail.com>
2021-02-21 20:26:43 -0700

    Spaces/tab formatting

--- main.js
+++ main.js
@@ -3,28 +3,28 @@ var txt = "Hi there! I'm";
 var speed = 50;
 
 function typeText() {
-  if (i < txt.length) {
-    document.getElementById("hiDesc").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeText, speed);
-  }
+    if (i < txt.length) {
+        document.getElementById("hiDesc").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeText, speed);
+    }
 }
 
 function hamburgerMenu() {
-  var x = document.getElementById("navbar");
-  if (x.className == "navbar") {
-    x.className += " responsive";
-    document.getElementById("hamburgerMenuImage").src = "images/exit.svg"
-  } else {
-    x.className = "navbar";
-    document.getElementById("hamburgerMenuImage").src = "images/hamburger.svg"
-  }
+    var x = document.getElementById("navbar");
+    if (x.className == "navbar") {
+        x.className += " responsive";
+        document.getElementById("hamburgerMenuImage").src = "images/exit.svg"
+    } else {
+        x.className = "navbar";
+        document.getElementById("hamburgerMenuImage").src = "images/hamburger.svg"
+    }
 }
 
 document.addEventListener("adobe_dc_view_sdk.ready", function () {
-  var adobeDCView = new AdobeDC.View({ clientId: "a319c2c4b18a46beada55d907ca135d3", divId: "adobe-dc-view" });
-  adobeDCView.previewFile({
-    content: { location: { url: "Nathan Tang - Resume.pdf" } },
-    metaData: { fileName: "Nathan Tang - Resume.pdf" }
-  }, { embedMode: "IN_LINE" });
+    var adobeDCView = new AdobeDC.View({ clientId: "a319c2c4b18a46beada55d907ca135d3", divId: "adobe-dc-view" });
+    adobeDCView.previewFile({
+        content: { location: { url: "Nathan Tang - Resume.pdf" } },
+        metaData: { fileName: "Nathan Tang - Resume.pdf" }
+    }, { embedMode: "IN_LINE" });
 });
